4498666e030c4642ac4a46cf72c1db1758baf513
DanielNemchenko <danielnemchenko8@gmail.com>
2025-06-21 13:14:32 +0200

    exam-Semaphores exercise 2 done (exam finished)

--- exam_Semaphores_lab6/src/TrayCollectorExercise.scala
+++ exam_Semaphores_lab6/src/TrayCollectorExercise.scala
@@ -8,17 +8,42 @@ object Cafeteria :
   // SC-2: The worker cannot take a tray if there is no one available.
 
   private val NUM_STATIONS = 4
+  private var num_trays = 0
+  private val mutex = new Semaphore(1, true)
+  private val stationNotFull = new Semaphore(1, true) // 1 = station is not full, 0 = is full
+  private val stationNotEmpty = new Semaphore(0, true)
 
   def takeTray =
-  
+    stationNotEmpty.acquire()
+    mutex.acquire()
+    num_trays -= 1
     // Worker flushed the content
     println(s"Worker has taken from a tray")
+    if (num_trays == NUM_STATIONS - 1)
+      stationNotFull.release()
+      
+    if (num_trays > 0)
+      stationNotEmpty.release()   
+      
+    mutex.release()
     Thread.sleep(Random.nextInt(300))
+    
 
 
   def putTray(id: Int) =
-  
+    stationNotFull.acquire()
+    mutex.acquire()
+
+    num_trays += 1
     println(s"Student $id puts in a tray")
+    if (num_trays < NUM_STATIONS)
+      stationNotFull.release()
+      
+    if (num_trays == 1)
+      stationNotEmpty.release()
+
+    mutex.release()
+
 
 
 object MainCafeteria :
